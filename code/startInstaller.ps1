0e36bd48f28a5ae6581ad6cbee8e4bb04390223c
“Eskil <eskil.refsgaard@gmail.com>
2022-03-08 12:21:22 +0100

    fix: startInstaller.ps1, lagt til git install

--- code/startInstaller.ps1
+++ code/startInstaller.ps1
@@ -1,26 +1,26 @@
 #Kjør som administrator
 
-#Link til git
-#	iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mats-s/quick-setup/main/win-srv-setup.ps1'))
-
-
 #Går til bruker
 cd C:\Users\Admin
 
+#Språk
 Get-WinUserLanguageList
 Set-WinUserLanguageList -LanguageList nb-NO
 
+#For å kunne kjøre
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y powershell-core
 
-choco install -y sysinternals
+choco install -y powershell-core				#CHOOOOCO
 
-# - choco install -y git
+choco install -y sysinternals					#Mr Windows Expert
 
-#bginfo vil tydeligvis ikke fungere så legger til det senere
+choco install -y git						#Kan cmt bort
 
-#Gå inn i pwsh // eksempelvil pwsh.exe
+#bginfo vil tydeligvis ikke fungere så legger til det senere	#Check
 
-pwsh
+pwsh.exe							
+
+#iex ((New-Object System.Net.WebClient).DownloadString('https://gist.githubusercontent.com/Datakriger101/2aeece14caa9bd022a1587b91b73bfa4/raw/18447241314c9b4a1cd3b666e98094bc06f62660/PS_startInstaller.ps1'))
+
