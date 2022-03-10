a109b33f7433dde14144e7513c659a7f359c5929
HaraldMR <harald.martin@raanes.net>
2022-03-10 14:43:49 +0100

    Fix: removed add from dc1, has to be done manually

--- code/AddComputers.ps1
+++ code/AddComputers.ps1
@@ -1,22 +1,10 @@
 # Adds the computers to the AD.
-
-
-
-
-# !! This script is not yet complete !!
-
-
+# CL1 and SRV1 must currently be added to the AD from respective computers.
 
 # MGR
-Get-ADComputer "MGR" | 
-    Move-ADObject -TargetPath "OU=Adm,OU=Clients,DC=sec,DC=core"
-
-# Get-ADComputer "CL1" | 
-#    Move-ADObject -TargetPath "OU=Cons,OU=Clients,DC=sec,DC=core"
+Get-ADComputer "MGR" | Move-ADObject -TargetPath "OU=Adm,OU=Clients,DC=sec,DC=core"
 
 # CL1
-# Add-Computer -Credential $cred -DomainName sec.core `
-#    -OUPath 'OU=Cons,OU=Clients,DC=sec,DC=core' -PassThru -Verbose
+Add-Computer -Credential $cred -DomainName sec.core -OUPath 'OU=Cons,OU=Clients,DC=sec,DC=core' -PassThru -Verbose
 # SRV1
-# Add-Computer -Credential $cred -DomainName sec.core `
-#    -OUPath 'OU=Servers,DC=sec,DC=core' -PassThru -Verbose
+Add-Computer -Credential $cred -DomainName sec.core -OUPath 'OU=Servers,DC=sec,DC=core' -PassThru -Verbose
