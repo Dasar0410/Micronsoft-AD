3d9804c2ce4460ed90545ddb180e80329ff667db
“Eskil <eskil.refsgaard@gmail.com>
2022-03-16 15:34:04 +0100

    Fix: Now able to delete OUS, AllUsers, Clients, Servers

--- code/DeleteOU.ps1
+++ code/DeleteOU.ps1
@@ -1,7 +1,12 @@
 # Delete all OUS
 get-ADOrganizationalUnit -Identity "OU=AllUsers,DC=sec,DC=core" |
-  set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false
+set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false
+Remove-ADOrganizationalUnit -Identity "OU=AllUsers,DC=sec,DC=core" -Recursive
 
-  Remove-ADOrganizationalUnit -Identity "OU=AllUsers,DC=sec,DC=core" -Recursive
-  Remove-ADOrganizationalUnit -Identity "OU=Clients,DC=sec,DC=core" -Recursive
-  Remove-ADOrganizationalUnit -Identity "OU=Servers,DC=sec,DC=core" -Recursive
+get-ADOrganizationalUnit -Identity "OU=Clients,DC=sec,DC=core" |
+set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false
+Remove-ADOrganizationalUnit -Identity "OU=Clients,DC=sec,DC=core" -Recursive
+
+get-ADOrganizationalUnit -Identity "OU=Servers,DC=sec,DC=core" |
+set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false
+Remove-ADOrganizationalUnit -Identity "OU=Servers,DC=sec,DC=core" -Recursive
