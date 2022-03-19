63679e50377f85932b545cf65cada7ab29bc140d
HaraldMR <harald.martin@raanes.net>
2022-03-19 14:45:53 +0100

    New: Bakgrunner til GPOer

--- code/AddComputers.ps1
+++ code/AddComputers.ps1
@@ -1,10 +1,5 @@
 # Adds the computers to the AD.
-# CL1 and SRV1 must currently be added to the AD from respective computers.
 
-# MGR
 Get-ADComputer "MGR" | Move-ADObject -TargetPath "OU=Adm,OU=Clients,DC=micron,DC=soft"
-
-# CL1
-Add-Computer -Credential $cred -DomainName micron.soft -OUPath "OU=Cons,OU=Clients,DC=micron,DC=soft" -PassThru -Verbose
-# SRV1
-Add-Computer -Credential $cred -DomainName micron.soft -OUPath 'OU=Servers,DC=micron,DC=soft' -PassThru -Verbose
+Get-ADComputer "CL1" | Move-ADObject -TargetPath "OU=Cons,OU=Clients,DC=micron,DC=soft"
+Get-ADComputer "SRV1" | Move-ADObject -TargetPath "OU=Servers,DC=micron,DC=soft"
