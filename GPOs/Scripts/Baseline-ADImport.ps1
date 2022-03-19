5564b71103e0241fd4b478a5af6dad26ad8f931b
HaraldMR <harald.martin@raanes.net>
2022-03-19 16:43:04 +0100

    Fix: more wrong paths

--- GPOs/Scripts/Baseline-ADImport.ps1
+++ GPOs/Scripts/Baseline-ADImport.ps1
@@ -2,7 +2,7 @@
 # Import all GPOs in this baseline into Active Directory Group Policy
 #
 
-$GpoMap = .\MapGuidsToGpoNames.ps1 ..\GPOs\GPOer
+$GpoMap = .\MapGuidsToGpoNames.ps1 ..\GPOer
 
 Write-Host "Importing the following GPOs:" -ForegroundColor Cyan
 Write-Host
@@ -12,7 +12,7 @@ Write-Host
 
 $rootDir = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
 $parentDir = [System.IO.Path]::GetDirectoryName($rootDir)
-$gpoDir = [System.IO.Path]::Combine($parentDir, "GPOs")
+$gpoDir = [System.IO.Path]::Combine($parentDir, "GPOer")
 
 $GpoMap.Keys | ForEach-Object {
     $key = $_
