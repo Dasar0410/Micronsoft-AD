10eed3b1fb1088103694cadd378f3221e036b60c
HaraldMR <harald.martin@raanes.net>
2022-03-19 15:51:47 +0100

    New: GPO backup and import scripts

---
+++ code/ImportGPOs.ps1
@@ -0,0 +1,34 @@
+choco install -y 7zip
+
+Set-Location 'C:\Users\Administrator'
+
+curl 'https://codeload.github.com/HaraldMR/DCSG1005downloadfork/legacy.zip/main' --output main.zip
+
+7z x '.\main.zip'
+Set-Location '.\HaraldMR-DCSG1005downloadfork-b80d563\GPOs\Scripts'
+Get-GPO -All | Format-Table -Property displayname 
+.\Baseline-ADImport.ps1
+Get-GPO -All | Format-Table -Property displayname
+
+Set-Location 'C:\Users\Administrator'
+
+Get-GPO -Name "[Adm] Bakgrunn" | New-GPLink -Target "OU=AllUsers,OU=Adm,DC=micron,DC=soft"
+Get-GPO -Name "[All Users] CMD off" | New-GPLink -Target "OU=AllUsers,DC=micron,DC=soft"
+Get-GPO -Name "[All Users] Firewall" | New-GPLink -Target "OU=AllUsers,DC=micron,DC=soft"
+Get-GPO -Name "[All Users] Forced system restarts off" | New-GPLink -Target "OU=AllUsers,DC=micron,DC=soft"
+Get-GPO -Name "[All Users] Password settings" | New-GPLink -Target "OU=AllUsers,DC=micron,DC=soft"
+Get-GPO -Name "[All Users] PIN Complexity" | New-GPLink -Target "OU=AllUsers,DC=micron,DC=soft"
+Get-GPO -Name "[All Users] Prohibit software installation" | New-GPLink -Target "OU=AllUsers,DC=micron,DC=soft"
+Get-GPO -Name "[All Users] Restrict access to settings" | New-GPLink -Target "OU=AllUsers,DC=micron,DC=soft"
+Get-GPO -Name "[Cons] Bakgrunn" | New-GPLink -Target "OU=AllUsers,OU=Cons,DC=micron,DC=soft"
+Get-GPO -Name "[Cons] Removable storage access off" | New-GPLink -Target "OU=AllUsers,OU=Cons,DC=micron,DC=soft"
+Get-GPO -Name "[Domain Policy] Anonymous SID enumeration disabled" | New-GPLink -Target "OU=Domain Controllers,DC=micron,DC=soft"
+Get-GPO -Name "[Domain Policy] Guest account access off" | New-GPLink -Target "OU=Domain Controllers,DC=micron,DC=soft"
+Get-GPO -Name "[HR] Bakgrunn" | New-GPLink -Target "OU=AllUsers,OU=HR,DC=micron,DC=soft"
+Get-GPO -Name "[IT] Access to settings" | New-GPLink -Target "OU=AllUsers,OU=IT,DC=micron,DC=soft"
+Get-GPO -Name "[IT] Allow software installations" | New-GPLink -Target "OU=AllUsers,OU=IT,DC=micron,DC=soft"
+Get-GPO -Name "[IT] Bakgrunn" | New-GPLink -Target "OU=AllUsers,OU=IT,DC=micron,DC=soft"
+Get-GPO -Name "[IT] CMD on" | New-GPLink -Target "OU=AllUsers,OU=IT,DC=micron,DC=soft"
+Get-GPO -Name "[IT] Forced system restarts on" | New-GPLink -Target "OU=AllUsers,OU=IT,DC=micron,DC=soft"
+
+Set-Location 'C:\Users\Administrator'
