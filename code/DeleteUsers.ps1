9a7c0eaaed6b1733a742a4da3d55cb0aa1f8dfa4
danie <daniepsa@stud.ntnu.no>
2022-03-20 02:42:26 +0100

    fix: fixed infinite loop bug

--- code/DeleteUsers.ps1
+++ code/DeleteUsers.ps1
@@ -1,18 +1,17 @@
 do{
 $valg = Read-Host "Slett en valgt bruker(1) / Slett ALLE brukere(2)"
-}while($valg -ne 1 && $valg -ne 2)
+}while($valg -lt 1 || $valg -gt 2)
 
 if ($valg -eq 1){ # Hvis en bruker skal slettes
-    $firstName = Read-Host "Fornavn på bruker som skal slettes"
-    $lastName = Read-Host "Etternavn på bruker som skal slettes"
+    $firstName = Read-Host "Fornavn paa bruker som skal slettes"
+    $lastName = Read-Host "Etternavn paa bruker som skal slettes"
     $fullName = $firstName + ' ' + $lastName
-    Get-ADUser -Filter "Name -like '$fullName'" | Remove-ADUser -Confirm:$false #spør ikke om konfirmasjon
-    Write-Output "Hvis bruker $fullname eksisterer så er den naa slettet"
-    
+    Get-ADUser -Filter "Name -like '$fullName'" | Remove-ADUser -Confirm:$false #spor ikke om konfirmasjon
+    Write-Output "Hvis bruker $fullname eksisterer saa er den naa slettet"
 }
-else if($valg -eq 2){# Hvis alle brukere skal slettes
+else {# Hvis alle brukere skal slettes
     Write-Output "Sletter ALLE brukere..."
-    Get-ADUser -SearchBase "OU=AllUsers, DC=micron, DC=soft" -Filter * | Remove-ADUser -Confirm #spør om konfirmasjon
+    Get-ADUser -SearchBase "OU=AllUsers, DC=micron, DC=soft" -Filter * | Remove-ADUser -Confirm #spor om konfirmasjon
     Write-Output "ALLE brukerne er slettet!"
 }
 
