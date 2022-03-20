7d7a7bf3e7564c1604c477c83f7f7aebf174c4ce
danie <daniepsa@stud.ntnu.no>
2022-03-20 16:31:52 +0100

    Added commenting for variables

--- code/MoveUserToOtherOU.ps1
+++ code/MoveUserToOtherOU.ps1
@@ -2,10 +2,10 @@ $OrgUnits = @("ou = Supp, ou = IT, ou = AllUsers, dc=micron, dc=soft","ou = ITad
         "ou = Prog, ou=Cons, ou=AllUsers, dc=micron, dc=soft","ou = Adm, ou = AllUsers, dc=micron, dc=soft", "ou = HR, ou = AllUsers, dc=micron, dc=soft")
 $firstName = Read-Host "Skriv inn fornavn pa bruker du vil flytte"
 $lastName = Read-Host "Skriv inn etternavn"
-$fullName = $firstName + ' ' + $lastName 
+$fullName = $firstName + ' ' + $lastName #legger til mellomrom så den vil funke med -Filter "Name"
 do{ # saa lenge OUvalg er en lovlig verdi
     $OUvalg = Read-Host "Velg OU(0-5): IT-Support[0], IT-Admin[1], Web-konsulent[2], Progutvikler[3], Administrasjon[4], HR[5]"
 }while($OUvalg -gt 5 || $OUvalg -lt 0)
-#hvis programkjorer har valgt en eksisterende bruker så blir den flyttet over til valgt OU
+#hvis programkjorer har valgt en eksisterende bruker sa blir den flyttet over til valgt OU
 Get-ADObject -Filter "Name -like '$fullName'" | Move-ADObject -TargetPath "$OrgUnits[$OUvalg]"
-Write-Output "Hvis $fullname eksisterer så er den flyttet"
+Write-Output "Hvis $fullname eksisterer sa er den flyttet"
