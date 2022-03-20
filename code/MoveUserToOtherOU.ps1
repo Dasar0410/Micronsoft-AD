8550fd5ed4a6dd501abcf2f13a05c4784d604a31
danie <daniepsa@stud.ntnu.no>
2022-03-20 17:04:35 +0100

    Fix: All scriptanalyzer problems removed

--- code/MoveUserToOtherOU.ps1
+++ code/MoveUserToOtherOU.ps1
@@ -2,7 +2,7 @@ $OrgUnits = @("ou = Supp, ou = IT, ou = AllUsers, dc=micron, dc=soft","ou = ITad
         "ou = Prog, ou=Cons, ou=AllUsers, dc=micron, dc=soft","ou = Adm, ou = AllUsers, dc=micron, dc=soft", "ou = HR, ou = AllUsers, dc=micron, dc=soft")
 $firstName = Read-Host "Skriv inn fornavn pa bruker du vil flytte"
 $lastName = Read-Host "Skriv inn etternavn"
-$fullName = $firstName + ' ' + $lastName #legger til mellomrom så den vil funke med -Filter "Name"
+$fullName = $firstName + ' ' + $lastName #legger til mellomrom sa den vil funke med -Filter "Name"
 do{ # saa lenge OUvalg er en lovlig verdi
     $OUvalg = Read-Host "Velg OU(0-5): IT-Support[0], IT-Admin[1], Web-konsulent[2], Progutvikler[3], Administrasjon[4], HR[5]"
 }while($OUvalg -gt 5 || $OUvalg -lt 0)
