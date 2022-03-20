56b17514b1442a1de4918109dba05bbafd000e4a
danie <daniepsa@stud.ntnu.no>
2022-03-20 02:31:46 +0100

    Fix: replaced ØÆÅ letters for scriptanalyzer

--- code/CreateSingleUser.ps1
+++ code/CreateSingleUser.ps1
@@ -1,10 +1,10 @@
 $OrgUnits = @("ou = Supp, ou = IT, ou = AllUsers","ou = ITadmin, ou = IT, ou = AllUsers","ou = Web, ou=Cons, ou=AllUsers",
             "ou = Prog, ou=Cons, ou=AllUsers","ou = Adm, ou = AllUsers", "ou = HR, ou = AllUsers")
 Write-Output "UserName;GivenName;SurName;UserPrincipalName;DisplayName;Password;Department;Path" > micronsoftusers.csv
-do{ # Denne løkken kjører så lenge brukeren vil legge til en ny bruker
+do{ # Denne lokken kjorer saa lenge brukeren vil legge til en ny bruker
     $firstName = Read-Host "Skriv inn fornavn"
     $lastName = Read-Host "Skriv inn etternavn"
-do{ # så lenge OUvalg er en lovlig verdi
+do{ # saa lenge OUvalg er en lovlig verdi
     $OUvalg = Read-Host "Velg OU(0-5): IT-Support[0], IT-Admin[1], Web-konsulent[2], Progutvikler[3], Administrasjon[4], HR[5]"
 }while($OUvalg -gt 5 || $OUvalg -lt 0)
 
@@ -20,7 +20,7 @@ Write-Output "$UserName;$GivenName;$SurName;$UserPrincipalName;$DisplayName;$Pas
 $Kjoreponytt = Read-Host "Vil du kjore igjen (j/N)"
 }while($Kjoreponytt -eq 'J')
 Write-Output "Legger til brukere..."
-## Når programkjører er ferdig med å legge til brukere så legges alle brukerne til AD nedenfor
+## Naar programkjorer er ferdig med a legge til brukere saa legges alle brukerne til AD nedenfor
 $ADUsers = Import-Csv micronsoftusers.csv -Delimiter ";"
 foreach ($User in $ADUsers) {
   New-ADUser `
